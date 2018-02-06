05138908fbf084d78c730aeb7a1243ef4c82fbb1
Chandler-Thompson <iamdrachir@gmail.com>
2018-02-06 16:47:56 -0500

    Messing with Bitmap
    Updated size of the bitmap, variabalized code, updated game description

--- Assignment 1 - GvG/js/main.js
+++ Assignment 1 - GvG/js/main.js
@@ -1,35 +1,23 @@
 "use strict";
 
 window.onload = function() {
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
+
+    let gaiaWidth = 256;
+    let gaiaHeight = 256;
+
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
-    }
-    
-    var bouncy;
+    function preload() {}
     
     function create() {
-        let bitWorld = game.add.bitmapData();
+        let bitWorld = game.add.bitmapData(gaiaWidth, gaiaHeight);
         bitWorld.ctx.beginPath();
-        bitWorld.ctx.rect(0,0,128,128);
+        bitWorld.ctx.rect(0,0,Camera.width,Camera.height);
         bitWorld.ctx.fillStyle = '#ff0000';
         bitWorld.ctx.fill();
 
-        let world = game.add.sprite(256, 256, bitWorld);
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
+        let world = game.add.sprite(gaiaWidth, gaiaHeight, bitWorld);
+
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
         var text = game.add.text( game.world.centerX, 15, "Gaia vs. Geologist", style );
         text.anchor.setTo( 0.5, 0.0 );
