fb68f4a074977a572903df5a0eae910a72fe0567
Chandler-Thompson <iamdrachir@gmail.com>
2018-02-06 16:17:55 -0500

    Added bitmap
    Simply added in a bitmap and removed other unnecessary template code.

--- Assignment 1 - GvG/js/main.js
+++ Assignment 1 - GvG/js/main.js
@@ -20,30 +20,22 @@ window.onload = function() {
     var bouncy;
     
     function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        let bitWorld = game.add.bitmapData();
+        bmd.ctx.beginPath();
+        bmd.ctx.rect(0,0,128,128);
+        bmd.ctx.fillStyle = '#ff0000';
+        bmd.ctx.fill();
+
+        let world = game.add.sprite(256, 256, bmd);
         
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
+        var text = game.add.text( game.world.centerX, 15, "Gaia vs. Geologist", style );
         text.anchor.setTo( 0.5, 0.0 );
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+        
     }
 };
